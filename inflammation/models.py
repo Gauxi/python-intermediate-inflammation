2b4fe77c2cb66ab634d2dd006f744462a2704bb6
margaux.gatrio <margaux.gatrio@bih-charite.de>
2024-06-24 12:21:25 +0200

    style: add docstrings

--- inflammation/models.py
+++ inflammation/models.py
@@ -19,16 +19,28 @@ def load_csv(filename):
 
 
 def daily_mean(data):
-    """Calculate the daily mean of a 2D inflammation data array."""
+    """Calculate the daily mean of a 2D inflammation data array.
+    
+    :param data: a 2D array
+    :return: the mean
+    """
     return np.mean(data, axis=0)
 
 
 def daily_max(data):
-    """Calculate the daily max of a 2D inflammation data array."""
+    """Calculate the daily max of a 2D inflammation data array.
+        
+    :param data: a 2D array
+    :return: the max
+    """
     return np.max(data, axis=0)
 
 
 def daily_min(data):
-    """Calculate the daily min of a 2D inflammation data array."""
+    """Calculate the daily min of a 2D inflammation data array.
+        
+    :param data: a 2D array
+    :return: the min
+    """
     return np.min(data, axis=0)
 
