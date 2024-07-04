3028fbbaf8945e988bfaa0f727ebd0c81caa436e
margaux.gatrio <margaux.gatrio@bih-charite.de>
2024-07-04 12:54:21 +0200

    Add more tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -61,7 +61,37 @@ def test_daily_min_string():
         error_expected = daily_min([['Hello', 'there'], ['General', 'Kenobi']])
 
 
-def patient_normalise(data):
-    """Normalise patient data from a 2D inflammation data array."""
-    max = np.max(data, axis=0)
-    return data / max[:, np.newaxis]
+@pytest.mark.parametrize(
+    "test, expected, expect_raises",
+    [
+        ([[0, 0, 0], [0, 0, 0], [0, 0, 0]], 
+         [[0, 0, 0], [0, 0, 0], [0, 0, 0]],
+         None,),
+        ([[1, 1, 1], [1, 1, 1], [1, 1, 1]], 
+         [[1, 1, 1], [1, 1, 1], [1, 1, 1]],
+         None,),
+        ([[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+         [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+         None,),
+        (
+            [[-1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            ValueError,
+        ),
+        (
+            [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
+            [[0.33, 0.67, 1], [0.67, 0.83, 1], [0.78, 0.89, 1]],
+            None,
+        ),
+    ])
+def test_patient_normalise(test, expected, expect_raises):
+    """Test normalisation works for arrays of one and positive integers."""
+    from inflammation.models import patient_normalise
+        
+    if expect_raises is not None:
+        with pytest.raises(expect_raises):
+            result = patient_normalise(np.array(test))
+            npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
+    else:
+        result = patient_normalise(np.array(test))
+        npt.assert_allclose(result, np.array(expected), rtol=1e-2, atol=1e-2)
