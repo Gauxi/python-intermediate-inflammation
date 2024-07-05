bf9cdd2beab470482699f009f5303f10a6f9ee3f
margaux.gatrio <margaux.gatrio@bih-charite.de>
2024-07-05 11:51:09 +0200

    decouples loading data

--- inflammation/compute_data.py
+++ inflammation/compute_data.py
@@ -14,13 +14,10 @@ def analyse_data(data_dir):
     Gets all the inflammation data from CSV files within a directory,
     works out the mean inflammation value for each day across all datasets,
     then plots the graphs of standard deviation of these means."""
-    data_file_paths = glob.glob(os.path.join(data_dir, 'inflammation*.csv'))
-    if len(data_file_paths) == 0:
-        raise ValueError(f"No inflammation data CSV files found in path {data_dir}")
-    data = map(models.load_csv, data_file_paths)
 
+    inflammation_data = load_inflammation_data(data_dir)
 
-    means_by_day = map(models.daily_mean, data)
+    means_by_day = map(models.daily_mean, inflammation_data)
     means_by_day_matrix = np.stack(list(means_by_day))
 
     daily_standard_deviation = np.std(means_by_day_matrix, axis=0)
@@ -29,3 +26,16 @@ def analyse_data(data_dir):
         'standard deviation by day': daily_standard_deviation,
     }
     views.visualize(graph_data)
+
+
+def load_inflammation_data(data_path):
+    """
+    Returns a list of 2D NumPy arrays with inflammation data loaded from all 
+    inflammation CSV files found in a specified directory path
+    """
+    data_file_paths = glob.glob(os.path.join(data_path, 'inflammation*.csv'))
+    if len(data_file_paths) == 0:
+        raise ValueError(f"No inflammation data CSV files found in path {data_path}")
+    data = list(map(models.load_csv, data_file_paths))
+
+    return data
